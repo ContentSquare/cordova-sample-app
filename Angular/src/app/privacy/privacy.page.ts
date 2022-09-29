23f991ab6bcbd665b70014a46cfe118e4bea6f6e
Romain VINCENT <romain.vincent@contentsquare-ext.com>
2022-09-29 14:55:55 +0200

    Add privacy feature

--- Angular/src/app/privacy/privacy.page.ts
+++ Angular/src/app/privacy/privacy.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentsquareCDVPlugin } from '@contentsquare/cordova-plugin-types';
+
+declare const ContentsquarePlugin: ContentsquareCDVPlugin;
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.page.html',
   styleUrls: ['./privacy.page.scss'],
 })
+
+// This class showcases how to use privacy-related APIs.
+// You are responsible for creating the UI asking the user for his consent, and then calling the appropriate Contentsquare function.
 export class PrivacyPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  optIn() {
+    ContentsquarePlugin.optIn(() => {
+      console.log('Opted in');
+    }, (err) => {
+      console.log('Opted out err =>', err);
+    });
+  }
+
+  optOut() {
+    ContentsquarePlugin.optOut(() => {
+      console.log('Opted out');
+    }, (err) => {
+      console.log('Opted out err =>', err);
+    });
   }
 
 }
