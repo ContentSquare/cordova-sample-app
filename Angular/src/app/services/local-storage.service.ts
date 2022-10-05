1593f746262076dcf8c9b823a2cb5d7913a87ad7
Romain VINCENT <romain.vincent@contentsquare-ext.com>
2022-10-05 16:35:24 +0200

    Add storage service

---
+++ Angular/src/app/services/local-storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  clearData() {
+    localStorage.clear();
+  }
+
+  getUserConsent(): boolean {
+    return localStorage.getItem('userConsent') === 'true';
+  }
+
+  acceptPolicy(){
+    localStorage.setItem('userConsent', 'true');
+  }
+
+  denyPolicy(){
+    localStorage.setItem('userConsent', 'false');
+  }
+
+  isFirstLaunch(){
+    return localStorage.getItem('isFirstLaunch') !== 'false';
+  }
+
+  onFirstLaunch(){
+    localStorage.setItem('isFirstLaunch', 'false');
+  }
+  
+}
