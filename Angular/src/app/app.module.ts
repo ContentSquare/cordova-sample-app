fabdd1b1128d788c991dc56578a40c90e5190a6a
Romain VINCENT <romain.vincent@contentsquare-ext.com>
2022-10-06 18:03:22 +0200

    Bug fix

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GpdrConsentComponent } from './components/gpdr-consent/gpdr-consent.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GpdrConsentComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
